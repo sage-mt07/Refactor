bfea9f2adacd3ddad9e2cd707deb839f3a009742
seijim <seiji7m@gmail.com>
2025-06-20 13:43:25 +0900

    up

--- src/Query/EventSets/EventQueryProvider.cs
+++ src/Query/EventSets/EventQueryProvider.cs
@@ -6,7 +6,7 @@ using System.Linq.Expressions;
 
 namespace KsqlDsl.Query.EventSets;
 
-internal class EventQueryProvider<T> : IQueryProvider
+internal class EventQueryProvider<T> : IQueryProvider where T : class
 {
     private readonly IKafkaContext _context;
     private readonly EntityModel _entityModel;
@@ -24,16 +24,48 @@ internal class EventQueryProvider<T> : IQueryProvider
 
         var elementType = expression.Type.GetGenericArguments().FirstOrDefault() ?? typeof(object);
 
+        // elementTypeがclass制約を満たすかチェック
+        if (!elementType.IsClass)
+        {
+            throw new ArgumentException($"Element type {elementType.Name} must be a reference type for EventSet<T>");
+        }
+
         var queryableType = typeof(EventSet<>).MakeGenericType(elementType);
-        return (IQueryable)Activator.CreateInstance(queryableType, _context, _entityModel, expression)!;
+
+        // IKafkaContextをKafkaContextにキャスト
+        var kafkaContext = _context as KafkaContext ??
+            throw new InvalidOperationException("Context must be of type KafkaContext");
+
+        return (IQueryable)Activator.CreateInstance(queryableType, kafkaContext, _entityModel, expression)!;
     }
 
-    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+    // 明示的なインターフェイス実装 - 型制約の問題を回避
+    IQueryable<TElement> IQueryProvider.CreateQuery<TElement>(Expression expression)
     {
         if (expression == null)
             throw new ArgumentNullException(nameof(expression));
 
-        return new EventSet<TElement>(_context, _entityModel, expression);
+        // TElementがclass制約を満たすかチェック
+        if (!typeof(TElement).IsClass)
+        {
+            throw new ArgumentException($"Element type {typeof(TElement).Name} must be a reference type for EventSet<T>");
+        }
+
+        // IKafkaContextをKafkaContextにキャスト
+        var kafkaContext = _context as KafkaContext ??
+            throw new InvalidOperationException("Context must be of type KafkaContext");
+
+        // リフレクションを使用してEventSet<TElement>を作成
+        var eventSetType = typeof(EventSet<>).MakeGenericType(typeof(TElement));
+        var createMethod = eventSetType.GetMethod("Create", new[] { typeof(KafkaContext), typeof(EntityModel), typeof(Expression) });
+
+        if (createMethod == null)
+        {
+            // Createメソッドが見つからない場合はコンストラクタを使用
+            return (IQueryable<TElement>)Activator.CreateInstance(eventSetType, kafkaContext, _entityModel, expression)!;
+        }
+
+        return (IQueryable<TElement>)createMethod.Invoke(null, new object[] { kafkaContext, _entityModel, expression })!;
     }
 
     public object? Execute(Expression expression)
